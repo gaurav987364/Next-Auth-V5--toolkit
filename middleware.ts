79d25c33366a8ddf457fc8759f8dd4c1b92bfa1f
gaurav <145903987+gaurav987364@users.noreply.github.com>
2025-05-10 17:09:07 +0530

    fix: add missing eslint-disable comment for unused variable in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import authConfig from "./auth.config"
 import NextAuth from "next-auth"
 
 const { auth } = NextAuth(authConfig) 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default auth((req) => {
   // req.auth
 })
